9fcf54d45c5cc940259e92437ab2df603608ecae
okuzawats <okuzawats@gmail.com>
2018-07-02 22:10:39 +0900

    UIBarButtonItemのタップイベントを修正

--- todoapp/ViewController.swift
+++ todoapp/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var dataSource: TaskDataSource!
+    var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        dataSource = TaskDataSource()
+        
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(TaskListCell.self, forCellReuseIdentifier: "Cell")
+        view.addSubview(tableView)
+        
+        let barButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(barButtonTapped(_:)))
+        navigationItem.rightBarButtonItem = barButton
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        dataSource.loadData()
+        tableView.reloadData()
+    }
+    
+    @objc func barButtonTapped(_ sender: UIBarButtonItem) {
+        // タスク作成画面へ画面遷移
+        //let controller = CreateTaskViewController()
+        //present(controller, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 68
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TaskListCell
+        let task = dataSource.data(at: indexPath.row)
+        
+        cell.task = task
+        return cell
+    }
+}
